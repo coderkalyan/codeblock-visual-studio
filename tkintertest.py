0754fdf382321fb7825f976be8f21628cb304725
bbworld <vw1212@pleasantonusd.net>
2018-07-04 18:18:54 -0700

    Did more testing with error catcher, will need a lot more work

---
+++ tkintertest.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+root = Tk()
+root.title("Tkinter Test GUI!")
+root.geometry('350x200')
+
+def peekaboo():
+    label2.grid(column=0, row=2)
+
+
+label1 = Label(root, text="Hello!", font=("Ubuntu", 45))
+label1.grid(column=0, row=0)
+
+button = Button(root, command=peekaboo, text="Press me!", font=("Ubuntu", 25))
+button.grid(column=0, row=1)
+
+label2 = Label(root, text="Peek-a-boo!")
+label2.grid(column=0, row=2)
+label2.grid_forget()
+
+root.mainloop()
