c62d127facbfb9017daee5f6f8ff22429c7847f1
bbworld <vw1212@pleasantonusd.net>
2018-07-05 21:41:58 -0700

    Wrote detectors for errors and warnings, and started on scan imports function

--- error_catcher.py
+++ error_catcher.py
@@ -1,20 +1,38 @@
 import sys, os
 import pylint.lint
+import importlib
 
-def get_errors(file):
+def get_lint(file):
     lint = os.popen("python3 -m flake8 " + file).read()
 
     errors_to_return = {}
+    warnings_to_return = {}
     for i in lint.split("\n"):
         try:
             print(i)
-            errors_to_return[i] = i.split(":")[1]
+            if any(error in i for error in ["F4", "F8", "F901", "E999"]):
+                if "F403" in i:
+                    scan_import(i)
+
+                errors_to_return[i] = int(i.split(":")[1])
+            else:
+                warnings_to_return[i] = int(i.split(":")[1])
         except IndexError:
             pass
     return errors_to_return
 
-def scan_imports(lines):
-    pass
+
+def scan_import(line):
+    print(line, "KABOOOOOM")
+    print(line.split("'")[1])
+    line = line.split("'")[1]
+    try:
+        if line.startswith("from"):
+            importlib.import_module() # import package
+        else:
+            importlib.import_module(
+                    line.split("import")[-1]) # import module
+
 
 if __name__ == "__main__":
-    print(get_errors("tkintertest.py"))
+    print(get_lint("tkintertest.py"))
