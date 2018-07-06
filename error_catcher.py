7653ddd4907bbb894ea1598efed9bdf5def70082
bbworld <vw1212@pleasantonusd.net>
2018-07-05 18:21:22 -0700

    Did a LOT of linter testing (now using flake8 due to licensing issues with Pylint)

--- error_catcher.py
+++ error_catcher.py
@@ -1,9 +1,20 @@
 import sys, os
+import pylint.lint
 
 def get_errors(file):
-    lint = os.popen("flake8 " + file).read()
+    lint = os.popen("python3 -m flake8 " + file).read()
 
-    print(lint)
+    errors_to_return = {}
+    for i in lint.split("\n"):
+        try:
+            print(i)
+            errors_to_return[i] = i.split(":")[1]
+        except IndexError:
+            pass
+    return errors_to_return
+
+def scan_imports(lines):
+    pass
 
 if __name__ == "__main__":
-    get_errors("tkintertest.py")
+    print(get_errors("tkintertest.py"))
